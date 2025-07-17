34b1e54ad886e94b0befb7277bc26496b0250f14
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-07-16 20:26:10 -0600

    feat: Refactor ProductSkeleton component for improved layout and styling

--- src/components/ProductSkeleton.tsx
+++ src/components/ProductSkeleton.tsx
@@ -1,23 +1,53 @@
-import { Card } from '@heroui/card';
+import { Card, CardBody, CardFooter, CardHeader } from '@heroui/card';
 import { Skeleton } from '@heroui/skeleton';
 
 export default function ProductSkeleton() {
   return (
-    <Card className='w-[200px] space-y-5 p-4' radius='sm'>
-      <Skeleton className='rounded-sm'>
-        <div className='h-24 rounded-sm bg-default-300' />
-      </Skeleton>
-      <div className='space-y-3'>
-        <Skeleton className='w-3/5 rounded-sm'>
-          <div className='h-3 w-3/5 rounded-sm bg-default-200' />
+    <Card className='w-64 max-w-sm my-4' radius='none' shadow='sm'>
+      <CardHeader className='text-lg font-semibold flex flex-col items-center'>
+        {/* Product name skeleton */}
+        <Skeleton className='rounded-sm'>
+          <div className='h-6 w-32 rounded-sm bg-default-300' />
         </Skeleton>
-        <Skeleton className='w-4/5 rounded-sm'>
-          <div className='h-3 w-4/5 rounded-sm bg-default-200' />
+      </CardHeader>
+
+      <CardBody>
+        {/* Product image skeleton */}
+        <Skeleton className='rounded-sm'>
+          <div className='w-full h-80 rounded-sm bg-default-300' />
         </Skeleton>
-        <Skeleton className='w-2/5 rounded-sm'>
-          <div className='h-3 w-2/5 rounded-sm bg-default-300' />
-        </Skeleton>
-      </div>
+      </CardBody>
+
+      <CardFooter className='flex flex-col gap-2 items-center'>
+        {/* Product description skeleton */}
+        <div className='space-y-2 w-full'>
+          <Skeleton className='rounded-sm'>
+            <div className='h-3 w-full rounded-sm bg-default-200' />
+          </Skeleton>
+          <Skeleton className='rounded-sm'>
+            <div className='h-3 w-4/5 rounded-sm bg-default-200' />
+          </Skeleton>
+          <Skeleton className='rounded-sm'>
+            <div className='h-3 w-3/5 rounded-sm bg-default-200' />
+          </Skeleton>
+        </div>
+
+        {/* Price and actions skeleton */}
+        <div className='flex justify-between w-full items-center mt-2'>
+          <Skeleton className='rounded-sm'>
+            <div className='h-6 w-16 rounded-sm bg-default-300' />
+          </Skeleton>
+          
+          <div className='flex gap-4'>
+            <Skeleton className='rounded-sm'>
+              <div className='h-8 w-8 rounded-sm bg-default-300' />
+            </Skeleton>
+            <Skeleton className='rounded-sm'>
+              <div className='h-6 w-6 rounded-sm bg-default-300' />
+            </Skeleton>
+          </div>
+        </div>
+      </CardFooter>
     </Card>
   );
 }
